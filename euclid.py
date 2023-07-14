53d2e050ac201964f080a90642660e77f002c1cc
touyunokoya <yuutototon@icloud.com>
2023-07-14 22:18:58 +0900

    Update euclid.py

--- euclid.py
+++ euclid.py
@@ -2,3 +2,19 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
+
+aa=int(a)
+bb=int(b)
+firsta =aa
+firstb =bb
+if bb>aa:
+    bb = firsta
+    aa = firstb
+while True:
+    q=aa//bb
+    r=aa-bb*q
+    if r==0:
+        print('{}と{}の最大公約数は{}です。'.format(firsta,firstb,bb))
+        break
+    aa=bb
+    bb=r 
