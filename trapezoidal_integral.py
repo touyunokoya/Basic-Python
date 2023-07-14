a06959cbb1075624063a753a8abcf42635821b4d
touyunokoya <yuutototon@icloud.com>
2023-07-14 22:18:34 +0900

    Update trapezoidal_integral.py

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,12 @@
 from math import sin
+import math
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
+k=0
+a=0
+b=math.pi/2
+h=(b-a)/100
+area = sum((h/2)*((sin(a+(k-1)*h))+sin(a+(k*h)))  for k in range(1,101))
+print(area)
